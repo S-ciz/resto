f31f8bc9727a86d83f3dc2a907b79081032b434c
S-ciz <95995178+S-ciz@users.noreply.github.com>
2021-12-14 08:19:07 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+
+var kit = [
+
+    //bullyJuice
+{catergory:"Full Body", coach:'Bully Juice',
+ vid:'<iframe width="500"  autoplay="true" height="315"  src="https://www.youtube.com/embed/oAPCPjnU1wA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> '},
+
+{catergory:"Abs", coach:'Bully Juice',
+ vid:'<iframe width="500"  autoplay="true" height="315" src="https://www.youtube.com/embed/1rWOUQnIXME" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+{catergory:"Arms", coach:'Bully Juice',
+ vid:'<iframe width="500"  autoplay="true" height="315" src="https://www.youtube.com/embed/JUQM3epPGcE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  '},
+//Chris Heria
+{catergory:'Full Body', coach:'Chris Heria', 
+vid:'<iframe width="500"  autoplay="true" height="315" src="https://www.youtube.com/embed/g30vNrUdOZg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+
+{catergory:'Abs', coach:'Chris Heria',
+ vid:'<iframe width="500"  autoplay="true" height="315" src="https://www.youtube.com/embed/1I7Z-8shzsU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+
+{catergory:'Arms', coach:'Chris Heria', vid:'<iframe width="500"  autoplay="true" height="315" src="https://www.youtube.com/embed/DrUEFnF5kG4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+
+{catergory:'Full Body', coach:'Igor Voitenko', 
+vid:'<iframe width="500"  autoplay="true" height="315" src="https://www.youtube.com/embed/2bHZ6ht3SyU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+{catergory:'Abs',coach:'Igor Voitenko',
+vid:'<iframe width="500"  autoplay="true" height="315" src="https://www.youtube.com/embed/R8oZk2wnpY0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+{catergory:'Arms',coach:'Igor Voitenko',
+vid:'<iframe width="500"  autoplay="true" height="315" src="https://www.youtube.com/embed/B8welbW2LiE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+
+    
+];
+
+  
+
+var coaches = [
+    {name: 'Chris Heria', pic:'/img/chris.jfif'},
+    {name: 'Bully Juice', pic:'/img/bully.jpg'},
+    {name:'Igor Voitenko', pic:'/img/igo.jfif'}
+
+ ];
+
+  
+
+
+export default {kit, coaches};
+
+
